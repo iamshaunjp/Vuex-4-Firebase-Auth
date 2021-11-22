7a9c51c4f63276b1f6f92777160420cf817c67bb
Shaun <shaun@thenetninja.co.uk>
2021-11-22 11:56:20 +0000

    lesson-8

--- my-blog/src/store/index.js
+++ my-blog/src/store/index.js
@@ -3,7 +3,8 @@ import { createStore } from 'vuex'
 // firebase imports
 import { auth } from '../firebase/config'
 import {
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from 'firebase/auth'
 
 const store = createStore({
@@ -25,6 +26,16 @@ const store = createStore({
         context.commit('setUser', res.user)
       } else {
         throw new Error('could not complete signup')
+      }
+    },
+    async login(context, { email, password }) {
+      console.log('login action')
+
+      const res = await signInWithEmailAndPassword(auth, email, password)
+      if (res) {
+        context.commit('setUser', res.user)
+      } else {
+        throw new Error('could not complete login')
       }
     }
   }
